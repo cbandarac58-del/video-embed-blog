662fad78be2c4c1751f7da42804a5f3c780876ba
cbandarac58-del <cbandarac58@gmail.com>
2026-05-24 12:38:29 -0700

    create dynamic js sitemap endpoint

---
+++ src/pages/sitemap.xml.js
@@ -0,0 +1,30 @@
+import fs from 'fs';
+import path from 'path';
+
+export async function GET() {
+  // Read our centralized database.json
+  const jsonPath = path.resolve('src/content/videos/database.json');
+  const fileData = fs.readFileSync(jsonPath, 'utf-8');
+  const videos = JSON.parse(fileData);
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://vixtube.net/</loc>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+  ${videos.map((video) => `
+  <url>
+    <loc>https://vixtube.net/videos/${video.slug}</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>`).join('')}
+</urlset>`.trim();
+
+  return new Response(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml; charset=utf-8'
+    }
+  });
+}
