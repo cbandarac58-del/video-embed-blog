15b29a4a631b427d9d342f05b9b429980347567e
cbandarac58-del <cbandarac58@gmail.com>
2026-09-06 21:09:46 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'static',
   site: 'https://vixtube.net',
-  prefetch: {
-    prefetchAll: false
-  }
 });
