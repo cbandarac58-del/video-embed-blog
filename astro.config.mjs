51aeab3e97ae7043a319b18ab4b7c23222623675
cbandarac58-del <cbandarac58@gmail.com>
2026-09-06 07:33:47 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
+  adapter: cloudflare(),
   site: 'https://vixtube.net',
   prefetch: {
     prefetchAll: false
